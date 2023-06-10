8caa495e71042d42ffad4fa3f6d54f888c3cb138
Lucas Barreto Barbosa <lucas.barbosa275@etec.sp.gov.br>
2023-06-10 13:03:48 -0300

    1.3

--- app/src/main/java/com/example/supermarioworldguide/PersonagensAct.java
+++ app/src/main/java/com/example/supermarioworldguide/PersonagensAct.java
@@ -2,13 +2,30 @@ package com.example.supermarioworldguide;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class PersonagensAct extends AppCompatActivity {
 
-    @Override
+    private Button voltar2;
+
+    public PersonagensAct() {
+    }
+
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_personagens);
+        this.setContentView(R.layout.activity_personagens);
+        this.voltar2 = (Button)this.findViewById(R.id.voltar2);
+        this.voltar2.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) { PersonagensAct.this.openMainActivity();
+            }
+        });
+    }
+
+    public void openMainActivity() {
+        Intent intent = new Intent(this, MainActivity.class);
+        this.startActivity(intent);
     }
 }
