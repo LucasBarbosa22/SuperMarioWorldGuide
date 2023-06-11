2e6d935ad7bdc0f9187297f645419ec5c802091f
Lucas Barreto Barbosa <lucas.barbosa275@etec.sp.gov.br>
2023-06-11 20:12:18 -0300

    1.4

--- app/src/main/java/com/example/supermarioworldguide/HistoriaAct.java
+++ app/src/main/java/com/example/supermarioworldguide/HistoriaAct.java
@@ -2,13 +2,30 @@ package com.example.supermarioworldguide;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class HistoriaAct extends AppCompatActivity {
 
-    @Override
+    private Button voltar3;
+
+    public HistoriaAct() {
+    }
+
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_historia);
+        this.setContentView(R.layout.activity_historia);
+        this.voltar3 = (Button)this.findViewById(R.id.voltar3);
+        this.voltar3.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) { HistoriaAct.this.openMainActivity();
+            }
+        });
+    }
+
+    public void openMainActivity() {
+        Intent intent = new Intent(this, MainActivity.class);
+        this.startActivity(intent);
     }
 }
