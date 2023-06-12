0e71cc63c93c15933ea7d286f3f46782399672b7
Lucas Barreto Barbosa <lucas.barbosa275@etec.sp.gov.br>
2023-06-11 23:07:52 -0300

    1.5

--- app/src/main/java/com/example/supermarioworldguide/MundosAct.java
+++ app/src/main/java/com/example/supermarioworldguide/MundosAct.java
@@ -2,13 +2,30 @@ package com.example.supermarioworldguide;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MundosAct extends AppCompatActivity {
 
-    @Override
+    private Button voltar4;
+
+    public MundosAct() {
+    }
+
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_mundos);
+        this.setContentView(R.layout.activity_mundos);
+        this.voltar4 = (Button)this.findViewById(R.id.voltar4);
+        this.voltar4.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) { MundosAct.this.openMainActivity();
+            }
+        });
+    }
+
+    public void openMainActivity() {
+        Intent intent = new Intent(this, MainActivity.class);
+        this.startActivity(intent);
     }
 }
